453cfcb32f3815ba8a3b30910e1911e9e480e38a
Febrian Bayu Nugroho <fbrnngrh@gmail.com>
2023-12-10 21:33:12 +0800

    feat: setup migration, controller and models

---
+++ database/migrations/2023_12_10_130254_create_invitations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->id();
+            $table->integer('theme_id')->unsigned();
+            $table->string('title_invitation')->nullable();
+            $table->string('name')->nullable();
+            $table->string('code')->nullable();
+            $table->string('thumbnail_img')->nullable();
+            $table->string('language')->nullable();
+            $table->string('url')->nullable();
+            $table->text('description')->nullable();
+            $table->date('date_invitation');
+            $table->time('time_invitation');
+            $table->string('timezone')->nullable();
+            $table->string('address_invitation')->nullable();
+            $table->string('address_invitation')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invitations');
+    }
+};
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -22,18 +22,117 @@
                 <svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24" viewBox="0 0 24 24"> <path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path> </svg>
             </div>
             <ul class="main-menu">
+
+            <!-- Start::slide__category -->
+            <li class="slide__category"><span class="category-name">User</span></li>
+            <!-- End::slide__category -->
+                
+            <!-- Start::slide -->
+              <li class="slide">
+                <a href="{{ route('admin-dashboard') }}" class="side-menu__item">
+                    <i class="fe fe-home side-menu__icon"></i>
+                    <span class="side-menu__label">Dashboard</span>
+                </a>
+            </li>
+            <!-- End::slide -->
+    
+
+             <!-- Start::slide__category -->
+             <li class="slide__category"><span class="category-name">CMS</span></li>
+             <!-- End::slide__category -->
+
+            {{-- Menu CMS --}}
+              <!-- Start::slide -->
+              <li class="slide has-sub">
+                <a href="javascript:void(0);" class="side-menu__item">
+                    <i class="fe fe-slack side-menu__icon"></i>
+                    <span class="side-menu__label">Manage Theme</span>
+                    <i class="fe fe-chevron-right side-menu__angle"></i>
+                </a>
+                <ul class="slide-menu child1">
+                    <li class="slide side-menu__label1">
+                        <a href="javascript:void(0)">Theme</a>
+                    </li>
+                    <li class="slide">
+                        <a href="full-calendar.html" class="side-menu__item">All Theme</a>
+                    </li>
+                </ul>
+            </li>
+            <!-- End::slide -->
+              <!-- Start::slide -->
+              <li class="slide has-sub">
+                <a href="javascript:void(0);" class="side-menu__item">
+                    <i class="fe fe-slack side-menu__icon"></i>
+                    <span class="side-menu__label">Manage Gallery</span>
+                    <i class="fe fe-chevron-right side-menu__angle"></i>
+                </a>
+                <ul class="slide-menu child1">
+                    <li class="slide side-menu__label1">
+                        <a href="javascript:void(0)">Galery</a>
+                    </li>
+                    <li class="slide">
+                        <a href="full-calendar.html" class="side-menu__item">All Galery</a>
+                    </li>
+                </ul>
+            </li>
+            <!-- End::slide -->
+              <!-- Start::slide -->
+              <li class="slide has-sub">
+                <a href="javascript:void(0);" class="side-menu__item">
+                    <i class="fe fe-slack side-menu__icon"></i>
+                    <span class="side-menu__label">Manage Assets</span>
+                    <i class="fe fe-chevron-right side-menu__angle"></i>
+                </a>
+                <ul class="slide-menu child1">
+                    <li class="slide side-menu__label1">
+                        <a href="javascript:void(0)">Assets</a>
+                    </li>
+                    <li class="slide">
+                        <a href="full-calendar.html" class="side-menu__item">All Assets</a>
+                    </li>
+                </ul>
+            </li>
+            <!-- End::slide -->
+              <!-- Start::slide -->
+              <li class="slide has-sub">
+                <a href="javascript:void(0);" class="side-menu__item">
+                    <i class="fe fe-slack side-menu__icon"></i>
+                    <span class="side-menu__label">Manage Sound Bank</span>
+                    <i class="fe fe-chevron-right side-menu__angle"></i>
+                </a>
+                <ul class="slide-menu child1">
+                    <li class="slide side-menu__label1">
+                        <a href="javascript:void(0)">Sound Bank</a>
+                    </li>
+                    <li class="slide">
+                        <a href="full-calendar.html" class="side-menu__item">All Sound Bank</a>
+                    </li>
+                </ul>
+            </li>
+            <!-- End::slide -->
+              <!-- Start::slide -->
+              <li class="slide has-sub">
+                <a href="javascript:void(0);" class="side-menu__item">
+                    <i class="fe fe-slack side-menu__icon"></i>
+                    <span class="side-menu__label">Manage Guestbook</span>
+                    <i class="fe fe-chevron-right side-menu__angle"></i>
+                </a>
+                <ul class="slide-menu child1">
+                    <li class="slide side-menu__label1">
+                        <a href="javascript:void(0)">Guestbook</a>
+                    </li>
+                    <li class="slide">
+                        <a href="full-calendar.html" class="side-menu__item">All Guestbook</a>
+                    </li>
+                </ul>
+            </li>
+            <!-- End::slide -->
+            {{-- End Menu CMS --}}
+
                 <!-- Start::slide__category -->
-                <li class="slide__category"><span class="category-name">Main</span></li>
+                <li class="slide__category"><span class="category-name">User</span></li>
                 <!-- End::slide__category -->
 
-                <!-- Start::slide -->
-                <li class="slide">
-                    <a href="{{ route('admin-dashboard') }}" class="side-menu__item">
-                        <i class="fe fe-home side-menu__icon"></i>
-                        <span class="side-menu__label">Dashboard</span>
-                    </a>
-                </li>
-                <!-- End::slide -->
 
                 <!-- Start::slide -->
                 <li class="slide">
