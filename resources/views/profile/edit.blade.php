eeefd04b1afb88388e1d55e9a8b7951d1c6ff8dd
anggieyulistiawan <anggieys140@gmail.com>
2024-02-03 22:46:12 +0800

    fix : button aksi profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -67,18 +67,26 @@
                                         <p class="mb-1 text-muted">Chief Executive Officer (C.E.O)</p>
                                         <p class="fs-12 op-7 mb-0">
                                             <span class="me-3 d-inline-flex align-items-center"><i
-                                                    class="ri-building-line me-1 align-middle"></i>Georgia</span>
+                                                    class="ri-building-line me-1 align-middle"></i>Tanah Laut</span>
                                             <span class="d-inline-flex align-items-center"><i
-                                                    class="ri-map-pin-line me-1 align-middle"></i>Washington
-                                                D.C</span>
+                                                    class="ri-map-pin-line me-1 align-middle"></i>Kalimatan Selatan</span>
                                         </p>
                                     </div>
                                 </div>
-                                
-                                {{-- <button class="btn btn-success" style="height: fit-content"><i
-                                        class="bi bi-pencil-square"></i> Edit Profile</button>
-                                <button class="btn btn" style="height: fit-content"><i
-                                        class="bi bi-pencil-square"></i> Edit Profile</button> --}}
+
+                                <div class="btn-group" style="height: fit-content">
+                                    <button type="button" class="btn btn-success dropdown-toggle" data-bs-toggle="dropdown"
+                                        aria-expanded="false">
+                                        Aksi
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="javascript:void(0);">Edit Profile</a></li>
+                                        {{-- <li>
+                                            <hr class="dropdown-divider">
+                                        </li> --}}
+                                        <li><a class="dropdown-item" href="javascript:void(0);">Ganti Password</a></li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                         <div class="card-body p-0 main-profile-info">
@@ -128,6 +136,14 @@
                                 <li class="list-group-item border-0">
                                     <div class="d-flex flex-wrap align-items-center">
                                         <div class="me-2 fw-semibold">
+                                            Umur :
+                                        </div>
+                                        <span class="fs-12 text-muted">18 Tahun</span>
+                                    </div>
+                                </li>
+                                <li class="list-group-item border-0">
+                                    <div class="d-flex flex-wrap align-items-center">
+                                        <div class="me-2 fw-semibold">
                                             Status :
                                         </div>
                                         <span class="fs-12 text-muted">Personal</span>
@@ -167,130 +183,6 @@
                                     <p class="mb-0">
                                         MIG-1-11, Monroe Street, Georgetown, Washington D.C, USA,20071 </p>
 
-                                </div>
-                            </div>
-                        </div>
-                        <div class="p-4 border-bottom border-block-end-dashed d-flex align-items-center">
-                            <p class="fs-15 mb-2 me-4 fw-semibold">Social Networks :</p>
-                            <div class="btn-list mb-0">
-                                <button class="btn btn-sm btn-icon btn-info-light btn-wave waves-effect waves-light">
-                                    <i class="ri-facebook-line"></i>
-                                </button>
-                                <button class="btn btn-sm btn-icon btn-secondary-light btn-wave waves-effect waves-light">
-                                    <i class="ri-twitter-line"></i>
-                                </button>
-                                <button class="btn btn-sm btn-icon btn-warning-light btn-wave waves-effect waves-light">
-                                    <i class="ri-instagram-line"></i>
-                                </button>
-                                <button class="btn btn-sm btn-icon btn-success-light btn-wave waves-effect waves-light">
-                                    <i class="ri-github-line"></i>
-                                </button>
-                                <button class="btn btn-sm btn-icon btn-danger-light btn-wave waves-effect waves-light">
-                                    <i class="ri-youtube-line"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xxl-6">
-                    <div class="row">
-                        <div class="col-xl-12">
-                            <div class=" custom-card">
-                                <div class="card-body p-0">
-                                    <div class="border-block-end-dashed  bg-white rounded-2 p-2">
-                                        <div>
-                                            <ul class="nav nav-pills nav-justified gx-3 tab-style-6 d-sm-flex d-block "
-                                                id="myTab" role="tablist">
-                                                <li class="nav-item rounded" role="presentation">
-                                                    <button class="nav-link" id="gallery-tab" data-bs-toggle="tab"
-                                                        data-bs-target="#gallery-tab-pane" type="button" role="tab"
-                                                        aria-controls="gallery-tab-pane" aria-selected="false"><i
-                                                            class="ri-exchange-box-line me-1 align-middle d-inline-block fs-16"></i>Gallery</button>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <div class="py-4">
-                                        <div class="tab-content" id="myTabContent">
-                                            <div class="tab-pane fade p-0 border-0" id="gallery-tab-pane" role="tabpanel"
-                                                aria-labelledby="gallery-tab" tabindex="0">
-                                                <div class="row">
-                                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                                                        <a href="../assets/images/media/media-40.jpg"
-                                                            class="glightbox card" data-gallery="gallery1">
-                                                            <img src="../assets/images/media/media-40.jpg" alt="image">
-                                                        </a>
-                                                    </div>
-                                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                                                        <a href="../assets/images/media/media-41.jpg"
-                                                            class="glightbox card" data-gallery="gallery1">
-                                                            <img src="../assets/images/media/media-41.jpg" alt="image">
-                                                        </a>
-                                                    </div>
-                                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                                                        <a href="../assets/images/media/media-42.jpg"
-                                                            class="glightbox card" data-gallery="gallery1">
-                                                            <img src="../assets/images/media/media-42.jpg" alt="image">
-                                                        </a>
-                                                    </div>
-                                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                                                        <a href="../assets/images/media/media-43.jpg"
-                                                            class="glightbox card" data-gallery="gallery1">
-                                                            <img src="../assets/images/media/media-43.jpg" alt="image">
-                                                        </a>
-                                                    </div>
-                                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                                                        <a href="../assets/images/media/media-44.jpg"
-                                                            class="glightbox card" data-gallery="gallery1">
-                                                            <img src="../assets/images/media/media-44.jpg" alt="image">
-                                                        </a>
-                                                    </div>
-                                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                                                        <a href="../assets/images/media/media-45.jpg"
-                                                            class="glightbox card" data-gallery="gallery1">
-                                                            <img src="../assets/images/media/media-45.jpg" alt="image">
-                                                        </a>
-                                                    </div>
-                                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                                                        <a href="../assets/images/media/media-46.jpg"
-                                                            class="glightbox card" data-gallery="gallery1">
-                                                            <img src="../assets/images/media/media-46.jpg" alt="image">
-                                                        </a>
-                                                    </div>
-                                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                                                        <a href="../assets/images/media/media-60.jpg"
-                                                            class="glightbox card" data-gallery="gallery1">
-                                                            <img src="../assets/images/media/media-60.jpg" alt="image">
-                                                        </a>
-                                                    </div>
-                                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                                                        <a href="../assets/images/media/media-26.jpg"
-                                                            class="glightbox card" data-gallery="gallery1">
-                                                            <img src="../assets/images/media/media-26.jpg" alt="image">
-                                                        </a>
-                                                    </div>
-                                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                                                        <a href="../assets/images/media/media-32.jpg"
-                                                            class="glightbox card" data-gallery="gallery1">
-                                                            <img src="../assets/images/media/media-32.jpg" alt="image">
-                                                        </a>
-                                                    </div>
-                                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                                                        <a href="../assets/images/media/media-30.jpg"
-                                                            class="glightbox card" data-gallery="gallery1">
-                                                            <img src="../assets/images/media/media-30.jpg" alt="image">
-                                                        </a>
-                                                    </div>
-                                                    <div class="col-lg-4 col-md-6 col-sm-12">
-                                                        <a href="../assets/images/media/media-31.jpg"
-                                                            class="glightbox card" data-gallery="gallery1">
-                                                            <img src="../assets/images/media/media-31.jpg" alt="image">
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
                                 </div>
                             </div>
                         </div>
