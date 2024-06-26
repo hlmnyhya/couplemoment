bc9bdf9a5854ec9439b432b08f65bc252c88d3f0
hlmnyhya <hlmnyhya99@gmail.com>
2024-06-26 15:52:14 +0800

    update with webhooks

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -9,11 +9,7 @@ class Gallery extends Model
 {
     use HasFactory;
 
-<<<<<<< HEAD
-    protected $fillable = ['gallery_name'];
-=======
     protected $fillable = ['invitation_id', 'gallery_name'];
->>>>>>> 7fa8e2330ce2d1ca7174b041708d2cf92c2c11e9
 
     public function photos()
     {
@@ -25,12 +21,9 @@ class Gallery extends Model
         // Get the total size of all photos in the gallery
         return $this->photos->sum->getFileSize();
     }
-<<<<<<< HEAD
-=======
 
     public function invitation()
     {
         return $this->belongsTo(Invitation::class, 'invitation_id');
     }
->>>>>>> 7fa8e2330ce2d1ca7174b041708d2cf92c2c11e9
 }
